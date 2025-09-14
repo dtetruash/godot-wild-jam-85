20a1e7fcb9c2d1faab196f5f748a98c69112b650
dtetruash <dtetruash@proton.me>
2025-09-14 23:11:48 +0200

    Add segment point setter

--- Rails/rail_setment/rail_segment.gd
+++ Rails/rail_setment/rail_segment.gd
@@ -6,6 +6,7 @@ extends Node3D
 		_regenerate_multimesh()
 		plank_interval = clampf(value, 0.3, 1.0)
 
+## Node whose childer's transforms will be used as points of the segment
 @export var path_guide: Node3D
 
 @onready var segment_path: Path3D = $SegmentPath
@@ -15,7 +16,25 @@ var is_mesh_dirty: bool = false
 func _ready() -> void:
 	_set_curve_points_from_guide()
 
+func set_segment_points(world_points: Array[Vector3]) -> void:
+	if not segment_path:
+		printerr("Tried setting rail segment world_points without a Path.")
+		return
+
+	var segment_curve := segment_path.curve
+	var point_count := world_points.size()
+
+	segment_curve.clear_points()
+
+	for point_idx in range(0, point_count):
+		segment_curve.add_point(world_points[point_idx])
+
+	CurveSmoothing.smooth(segment_curve)
+
 func _set_curve_points_from_guide():
+	if not path_guide:
+		return
+
 	var segment_curve := segment_path.curve
 	var point_count: int  = path_guide.get_child_count()
 
@@ -32,11 +51,6 @@ func _process(_delta: float) -> void:
 	if is_mesh_dirty:
 		_regenerate_multimesh()
 		is_mesh_dirty = false
-
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_mask == MOUSE_BUTTON_MASK_MIDDLE:
-			_set_curve_points_from_guide()
 
 func  _regenerate_multimesh():
 	if segment_path == null:
