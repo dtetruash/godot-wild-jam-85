57b3498a25531e287bea5f2b586fce2e013396d8
dtetruash <dtetruash@proton.me>
2025-09-21 15:55:59 +0200

    Update town labels to be 3d objects in the scene

--- Systems/town_management/town_label_button.gd
+++ Systems/town_management/town_label_button.gd
@@ -1,20 +1,17 @@
-class_name TownLabel extends Control
+class_name TownLabel extends Node3D
 
 @export var town_position: Vector3
 @export var town_name: String
 @export var town_id: int
 
-@onready var button = self.find_child("Button", true)
+@onready var button: Button = $TownLabelSkin/SubViewport/TownLabelUI/Button
 @onready var ui_manager = self.find_parent("UIContainer")
 
 func _ready() -> void:
 	button.pressed.connect(_on_button_pressed)
 
 func _process(delta):
-	var viewport = get_viewport()
 	# `town_global_pos` is the Vector3 position of the town in world space
-	var screen_pos = viewport.get_camera_3d().unproject_position(town_position)
-	self.position = screen_pos - self.size / 2  # center label above town
 	self.find_child("Label", true).text = self.town_name
 
 
