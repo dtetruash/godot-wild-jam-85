a7bb480b30602ebb45682af16080656cf30af8a8
David Simon Tetruashvili <dtetruash@proton.me>
2025-09-21 22:33:14 +0200

    Fix interaction (#55)

--- Systems/town_management/town_label_ui.gd
+++ Systems/town_management/town_label_ui.gd
@@ -1,13 +1,5 @@
 extends Control
 
-@onready var button: Button = $Button
-@onready var ui_manager = self.find_parent("UIContainer")
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	button.pressed.connect(_on_button_pressed)
-
-
-func _on_button_pressed() -> void:
-	print_debug("Clicked a town!")
-	ui_manager._on_town_clicked(self.town_id)
